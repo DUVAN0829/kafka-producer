2d9f55cdea1a9866a3d004c2c7beb2c2d424dad9
DUVAN0829 <duvancasgonz29@gmail.com>
2025-08-13 20:15:30 -0500

    Commit inicial :D

---
+++ src/main/java/co/duvan/kafka/producer/ProducerMsvcApplication.java
@@ -0,0 +1,13 @@
+package co.duvan.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerMsvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerMsvcApplication.class, args);
+    }
+
+}
